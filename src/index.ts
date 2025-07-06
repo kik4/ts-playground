0e978637c976b1e1c92d1b842dde74e46060bdc8
kik4 <kik4.contact@gmail.com>
2025-07-07 03:35:34 +0900

    sample date-fns-tz

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
+import { utcToZonedTime } from "date-fns-tz";
+
 console.log("🚀 TypeScript Playground");
+
+process.env.TZ = "UTC";
+
+const now = new Date("2025-07-07T00:00+09:00");
+
+const zonedTime = utcToZonedTime(now, "Asia/Tokyo");
+
+console.log({
+  offset: now.getTimezoneOffset(),
+  now,
+  zonedTime,
+});
 
 console.log("🚀 Done!");
